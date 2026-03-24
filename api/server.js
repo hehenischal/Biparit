88932ef0e75aef765a45903e7117529db546facd
Nischal lamichhane <108085352+hehenischal@users.noreply.github.com>
2026-03-25 01:12:01 +0545

    Revise HTML snippet generation rules for seriousness
    Updated prompt to generate a more serious and interactive HTML snippet.

--- api/server.js
+++ api/server.js
@@ -19,11 +19,13 @@ app.post('/api/server', async (req, res) => {
     }
 
     const prompt = `The user thinks this website is about: "${userGuess}". 
-    Generate a creative, interactive Bootstrap 5 HTML snippet that represents the EXACT OPPOSITE of that guess.
+    Generate a creative, interactive Bootstrap 5 HTML snippet that represents the OPPOSITE of that guess.
     Rules:
     1. Use modern Bootstrap 5 classes.
-    2. Provide ONLY the HTML code inside a <div>. No markdown backticks, no explanations. Give code in such a way that the website looks completely different each time. I will be replacing it using body.InnerHTML()
-    3. Make it ironic or funny.`;
+    2. Provide ONLY HTML,CSS,JS code to be replaced inside of Body tag(using InnerHTML). No markdown backticks, no explanations. Give code in such a way that the website looks great.
+    3. Make it super serious looking. nothing too obviously fake. User should read the contents to be able to know that its a joke.
+    4. Use CSS,JS or other things to make the website interactive. I am not doing any kind of sanitation & the website should feel modern. not just static garbage.
+    5. You can make Rage Baity website if theres an opportunity to do so.`;
 
     try {
         const result = await model.generateContent(prompt);
